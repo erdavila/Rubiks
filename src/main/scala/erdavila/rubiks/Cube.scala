090d7dbc6b11627b90bc6a42ccc44ce893318fc3
Eduardo R. D'Avila <erdavila@gmail.com>
2016-08-17 21:52:41 -0300

    Check for faces sizes and test Cube constructor and properties

--- src/main/scala/erdavila/rubiks/Cube.scala
+++ src/main/scala/erdavila/rubiks/Cube.scala
@@ -1,9 +1,18 @@
 package erdavila.rubiks
 
 class Cube(facesProvider: (FaceLabel) => Face) {
-  private val faces = FaceLabel.values
+  val faces = FaceLabel.values
     .map { f => f -> facesProvider(f) }
     .toMap
+
+  val size = {
+    val anyFace = faces.values.head
+    val sz = anyFace.size
+    if (!faces.values.forall(_.size == sz)) {
+      throw new IllegalArgumentException("All faces must have same size")
+    }
+    sz
+  }
 
   def rotateFace(faceLabel: FaceLabel, rotation: Rotation): Unit =
     faces(faceLabel).rotate(rotation)
--- src/test/scala/erdavila/rubiks/CubeSpec.scala
+++ src/test/scala/erdavila/rubiks/CubeSpec.scala
@@ -1,5 +1,6 @@
 package erdavila.rubiks
 
+import erdavila.rubiks.Color.White
 import erdavila.rubiks.FaceLabel._
 import erdavila.rubiks.Rotation.Clockwise
 
@@ -7,6 +8,47 @@ class MockableFace extends Face(0, 3, null)
 
 class CubeSpec extends UnitSpec {
   describe("Cube") {
+    describe("constructor") {
+      it("fails if the passed faces have unmatching sizes") {
+        val faces = Map[FaceLabel, Face](
+          F -> new Face(-1, 0, White),
+          U -> new Face(-1, 1, White),
+          R -> new Face(-1, 2, White),
+          B -> new Face(-1, 3, White),
+          D -> new Face(-1, 4, White),
+          L -> new Face(-1, 5, White))
+
+        an [IllegalArgumentException] should be thrownBy new Cube(faces)
+      }
+    }
+
+    describe(".faces") {
+      it("have the same faces passed to the constructor") {
+        val faces = Map[FaceLabel, Face](
+          F -> mock[MockableFace],
+          U -> mock[MockableFace],
+          R -> mock[MockableFace],
+          B -> mock[MockableFace],
+          D -> mock[MockableFace],
+          L -> mock[MockableFace])
+
+        val cube = new Cube(faces)
+
+        cube.faces should equal (faces)
+      }
+    }
+
+    describe(".size") {
+      it("is the same as the faces'") {
+        val theSameFace = new Face(0, 3, White)
+        val faces = FaceLabel.values.map(_ -> theSameFace).toMap
+
+        val cube = new Cube(faces)
+
+        cube.size should equal (3)
+      }
+    }
+
     describe(".rotateFace()") {
       it("rotates a face") {
         val rightFace = stub[MockableFace]
